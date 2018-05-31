70b3f97211eb325d85969db95d07c899d82314af
HKDemircan <hkdemircan06@gmail.com>
2018-05-31 17:06:46 +0300

    yorum satırı eklendi.

--- SimpleCaculator/app/src/main/java/com/hkadirdemircan/simplecaculator/MainActivity.java
+++ SimpleCaculator/app/src/main/java/com/hkadirdemircan/simplecaculator/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * tasarimimizda bulunan EditText ve TextView icin nesne olusturuyoruz.
+     * id'lerini bilgilerini bu degiskenlerde tutacagiz ve bununla
+     * islemler yapacagiz.
+     */
     EditText editText1;
     EditText editText2;
     TextView textView;
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //layout'da ki textlerimize erismek icin
+        //id lerini EditText tipindeki degiskenlerde tutuyorum.
         editText1 = findViewById(R.id.editText);
         editText2 = findViewById(R.id.editText2);
         textView = findViewById(R.id.resultText);
@@ -29,24 +36,46 @@ public class MainActivity extends AppCompatActivity {
         sayi1 = Integer.parseInt(editText1.getText().toString());
         sayi2 = Integer.parseInt(editText2.getText().toString());
     }
+
+    /**
+     * sum onclick oldugunda EditText'lere girilen
+     * degerleri alip gerekli islemi yapip textView'de gosteriyor.
+     * @param view
+     */
     public void sum(View view) {
         readText();
         int resultInt = sayi1 + sayi2;
         textView.setText("Result : " + resultInt);
     }
 
+    /**
+     * deduct onclick oldugunda EditText'lere girilen
+     * degerleri alip gerekli islemi yapip textView'de gosteriyor.
+     * @param view
+     */
     public void deduct(View view) {
         readText();
         int resultInt = sayi1 - sayi2;
         textView.setText("Result : " + resultInt);
     }
 
+    /**
+     *  multiply onclick oldugunda EditText'lere girilen
+     * degerleri alip gerekli islemi yapip textView'de gosteriyor.
+     * @param view
+     */
     public void multiply (View view) {
         readText();
         int resultInt = sayi1 * sayi2;
         textView.setText("Result : " + resultInt);
     }
 
+    /**
+     * divide onclick oldugunda EditText'lere girilen
+     * degerleri alip gerekli islemi yapip textView'de gosteriyor.
+     *
+     * @param view
+     */
     public void divide (View view) {
         readText();
         int resultInt = sayi1 / sayi2;
